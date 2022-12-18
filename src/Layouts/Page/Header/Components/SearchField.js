3f76f0882d9e1ff9628114b128c07b529fba0c3b
stacyhei <120314212+stacyhei@users.noreply.github.com>
2022-12-18 14:52:39 +0000

    update SearchField properties

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -9,21 +9,24 @@ const WrapperInput = styled.div`
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    border-radius: 24px;
+    background: #F8FAFC;
+    border: 1px solid #E2E8F0;
     color: #152842;
     width: 100%;
+    padding: 8px 16px;
+    gap: 16px;
+    
     &:hover {
-        background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+        opacity: 0.8;
     }
     &:focus {
-        background: white;
+        
     }
 `;
 
 const SearchIconWrapper = styled.div`
-    background-color: #4B759E;
+    
     position: absolute;
     box-sizing:border-box;
     right: 0px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
@@ -34,7 +37,7 @@ const SearchIconWrapper = styled.div`
 function SearchField() {
     return (
         <WrapperInput className='d-inline-flex'>  {/* Устанавливает ширину поля и включает относительное положение */}
-            <Input className='p-2' placeholder='Найти на странице...'/>
+            <Input placeholder='Найти'/>
             <SearchIconWrapper className='p-2'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
                 <IconOnlyButton icon={<SearchIcon/>}/>
             </SearchIconWrapper>
