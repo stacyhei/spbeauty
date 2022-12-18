86aa589dff146c8720bc1c45317bacb8b07034af
stacyhei <120314212+stacyhei@users.noreply.github.com>
2022-12-18 23:35:29 +0000

    update grid

--- src/Layouts/Page/AboutCompany/Components/Heading.js
+++ src/Layouts/Page/AboutCompany/Components/Heading.js
@@ -2,8 +2,8 @@ import PrimaryButton from "../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../Components/SecondaryButton";
 function Heading() {
     return (
-        <div className='d-flex flex-row justify-content-between mt-5'>
-            <h1 className='large-title'>Данные о компании</h1>
+        <div className='d-flex flex-row justify-content-around mt-5 '>
+            <h1 className='large-title col-6'>Данные о компании</h1>
             <SecondaryButton>Закрыть запись</SecondaryButton>
         </div>
     );
