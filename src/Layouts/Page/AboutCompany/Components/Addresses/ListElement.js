7159845c309a6638d7aa4844e4965f3c5910bdb7
stacyhei <120314212+stacyhei@users.noreply.github.com>
2022-12-22 20:19:55 +0000

    update typography

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -5,13 +5,13 @@ import RemoveIcon from '../../../../../Icons/RemoveIcon';
 import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
 
 const Option = styled.div`
-    
+    color: #0F172A;
 `;
 
 function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2 mt-3' >
-            <Option className='flex-grow-1'>{props.children}</Option>
+            <Option className='flex-grow-1 '>{props.children}</Option>
             
             <IconOnlyButton icon={<EditIcon/>}/>
             <IconOnlyButton icon={<RemoveIcon/>}/>
